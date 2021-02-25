6c43cbefc1525621d059fc90cc7fd103e9f60605
Alex Shvedov <alexandershvedov1997@gmail.com>
2021-02-25 22:41:45 +0300

    Add correctiont to the project

--- src/zadacha_8.py
+++ src/zadacha_8.py
@@ -2,7 +2,7 @@ import glob
 
 python_files = glob.glob('*.py')
 for file_name in sorted(python_files):
-    print('    ------' + file_name)
+    print('    ------'+file_name)
     with open(file_name) as f:
         for line in f:
             print('    ' + line.rstrip())
