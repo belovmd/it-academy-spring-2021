6327b6cdf20355f245ef24773270a577aeabb0b4
Maria <75201807+Lyouka@users.noreply.github.com>
2021-02-26 20:27:43 +0300

    Update 12.py

--- Homework 1/12.py
+++ Homework 1/12.py
@@ -1,11 +1,17 @@
 class BankAccount(object):
     def __init__(self, initial_balance=0):
         self.balance = initial_balance
-    def deposit(self, amount):
+
+
+def deposit(self, amount):
         self.balance += amount
-    def withdraw(self, amount):
+
+
+def withdraw(self, amount):
         self.balance -= amount
-    def overdrawn(self):
+
+
+def overdrawn(self):
         return self.balance < 0
 my_account = BankAccount(15)
 my_account.withdraw(50)
