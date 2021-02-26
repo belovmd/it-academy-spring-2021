53471ee99eef2a7d85906b3ffe22408b489bbedf
skolb1992 <s.kolb1992@yandex.ru>
2021-02-26 18:37:52 +0300

    fix

--- homework_1/8-Queens Problem recursion.py
+++ homework_1/8-Queens Problem recursion.py
@@ -3,10 +3,8 @@ BOARD_SIZE = 8
 
 def under_attack(col, queens):
     left = right = col
-
     for r, c in reversed(queens):
         left, right = left - 1, right + 1
-
         if c in (left, col, right):
             return True
     return False
@@ -15,13 +13,11 @@ def under_attack(col, queens):
 def solve(n):
     if n == 0:
         return [[]]
-
     smaller_solutions = solve(n - 1)
-
-    return [solution+[(n, i+1)]
+    return [solution + [(n, i + 1)]
         for i in range(BOARD_SIZE)
             for solution in smaller_solutions
-                if not under_attack(i+1, solution)]
+                if not under_attack(i + 1, solution)]
 
 
 for answer in solve(BOARD_SIZE):
