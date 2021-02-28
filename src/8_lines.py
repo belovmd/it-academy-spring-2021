132590fc5ce9f1f9b5a04df444ec5d5b781b7e34
sokolvladimir <sokolvladimir-by@mail.ru>
2021-02-28 11:24:40 +0300

    message2

--- src/8_lines.py
+++ src/8_lines.py
@@ -4,4 +4,4 @@ try:
     total = sum(int(arg) for arg in sys.argv[1:])
     print('sum =', total)
 except ValueError:
-    print('Please supply integer arguments')  
+    print('Please supply integer arguments')
