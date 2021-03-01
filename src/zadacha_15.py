00c26cbdc85fbeb54dac497fa6903adc3ccf0ea9
Alex Shvedov <alexandershvedov1997@gmail.com>
2021-03-01 19:42:01 +0300

    Add correction to file zadacha_15

--- src/zadacha_15.py
+++ src/zadacha_15.py
@@ -1,5 +1,6 @@
 BOARD_SIZE = 8
 
+
 def under_attack(col, queens):
     left = right = col
     for r, c in reversed(queens):
@@ -8,15 +9,14 @@ def under_attack(col, queens):
             return True
     return False
 
+
 def solve(n):
     if n == 0:
         return [[]]
     smaller_solutions = solve(n - 1)
-    return [solution+[(n, i + 1)]
-        for i in range(BOARD_SIZE)
-            for solution in smaller_solutions
-                if not under_attack(i + 1,
-                                    solution)]
+    return [solution + [(n, i + 1)] for i in range(BOARD_SIZE)
+            for solution in smaller_solutions if not under_attack(i + 1, solution)]
+
 
 for answer in solve(BOARD_SIZE):
     print(answer)
