d95a81adfb5b33092abf023fb996d706fa12b04d
nataliasipenko <natalia.asipenko@gmail.com>
2021-03-02 20:48:21 +0300

    eleventh

--- src/file_16.py
+++ src/file_16.py
@@ -14,12 +14,13 @@ def under_attack(col, queens):
 def solve(n):
     if n == 0:
         return [[]]
-
-    smaller_solutions = solve(n - 1)
+               smaller_solutions = solve(n - 1)
 
     return [solution + [(n, i + 1)]
         for i in range(BOARD_SIZE)
             for solution in smaller_solutions
                 if not under_attack(i + 1, solution)]
-            for answer in solve(BOARD_SIZE):
-                print(answer)
+
+
+for answer in solve(BOARD_SIZE):
+    print(answer)
