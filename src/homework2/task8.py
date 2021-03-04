d5f04177d2b86f5f5342bbcfd642eedb583a2c69
Maksim_Kozlov <maksimkozlov89@gmail.com>
2021-03-04 19:55:40 +0300

    hw2

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -1,6 +1,8 @@
 # In this mission, you need to create a password verification function.
 # The verification condition is:
 # the length should be bigger than 6.
+from typing import Iterable
+
 
 def is_acceptable_password(password: str) -> bool:
     if len(password) > 6:
@@ -30,3 +32,20 @@ def get_sum(a, b):
     for i in range(min_ab, max_ab + 1):
         summa = summa + i
     return summa
+
+
+# Remove All Before
+
+def remove_all_before(items: list, border: int) -> Iterable:
+    new_list = items
+    if not items:
+        return []
+    elif not items.count(border):
+        return items
+    else:
+        k = items.index(border)
+        i = 0
+        while i < k:
+            del new_list[0]
+            i = i + 1
+    return new_list
