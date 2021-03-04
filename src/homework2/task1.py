ce6745198fa0b346b0e27aeb7eae6cdfb060a3ee
Alex Shvedov <alexandershvedov1997@gmail.com>
2021-03-04 20:40:30 +0300

    Add new file task1.py

---
+++ src/homework2/task1.py
@@ -0,0 +1,28 @@
+def calculate_total_price(*price):
+    total_price = dict()
+    total_price["рубли"] = 0
+    total_price["копейки"] = 0
+    for current_element in price:
+        local_dict = parse_price(current_element)
+        total_price["рубли"] += local_dict["рубли"]
+        total_price["копейки"] += local_dict["копейки"]
+        while total_price["копейки"] >= 100:
+            count_ruble = total_price["копейки"] // 100
+            total_price["рубли"] += count_ruble
+            total_price["копейки"] = total_price["копейки"] % 100
+    string_result = "Общая цена {} рублей {} копеек".format(total_price["рубли"],total_price["копейки"] )
+    return string_result
+
+
+def parse_price(price_string):
+    lst = price_string.split(" ")
+    price = dict()
+    price["рубли"] = int(lst[0])
+    price["копейки"] = int(lst[2])
+    return price
+
+
+p1 = "3 рубля 20 копеек"
+p3 = "3 рубля 20 копеек"
+p5 = "3 рубля 20 копеек"
+print(calculate_total_price(p1, p3, p5))
