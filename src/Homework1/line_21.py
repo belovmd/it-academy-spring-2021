7de0b797fb44af87185badcf6cbc9f4065e7d755
Alexander Serzhan <serzhan1990@tut.by>
2021-03-06 22:38:13 +0300

    new

--- src/Homework1/line_21.py
+++ src/Homework1/line_21.py
@@ -1,4 +1,9 @@
-dinner_recipe = '''<html><body><table>
+# From http://effbot.org/zone/element-index.htm
+import xml.etree.ElementTree as etree
+tree = etree.fromstring(dinner_recipe)
+
+
+dinner_recipe: str = '''<html><body><table>
 <tr><th>amt</th><th>unit</th><th>item</th></tr>
 <tr><td>24</td><td>slices</td><td>baguette</td></tr>
 <tr><td>2+</td><td>tbsp</td><td>olive oil</td></tr>
@@ -6,9 +11,6 @@ dinner_recipe = '''<html><body><table>
 <tr><td>1</td><td>jar</td><td>pesto</td></tr>
 </table></body></html>'''
 
-# From http://effbot.org/zone/element-index.htm
-import xml.etree.ElementTree as etree
-tree = etree.fromstring(dinner_recipe)
 
 # For invalid HTML use http://effbot.org/zone/element-soup.htm
 # import ElementSoup, StringIO
