0675d0ed378386babd6ec691c199ff3d14447d81
alvik1975 <76609311+alvik1975@users.noreply.github.com>
2021-03-09 21:27:11 +0300

    Homework2: Aleksandr Misachenko

--- Homework2/task4.py
+++ Homework2/task4.py
@@ -1,9 +1,5 @@
 # Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке
-str = 'Python string method split() inputs a string value and ' \
-      'outputs a list of words contained within ' \
-      'the string by separating or splitting the words ' \
-      'on all the whitespaces by default. It also has an ' \
-      'optional Argument for limiting the number of splits.'
+str = input()
 big = 0
 small = 0
 
