a3e5ad1c075803fadb760100a32adec444fd5088
Dmitry Subbotin <sduslav@gmail.com>
2021-03-13 00:10:48 +0300

    fix task8.py

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -86,7 +86,7 @@ def snail(matrix):
 if __name__ == '__main__':
     print('------------ alphabet_position tests --------------')
     test = "The sunset sets at twelve o' clock."
-    assert alphabet_position(test) == ('20 8 5 19 21 14 19 5 20 19 5 20 19 ' 
+    assert alphabet_position(test) == ('20 8 5 19 21 14 19 5 20 19 5 20 19 '
                                        '1 20 20 23 5 12 22 5 15 3 12 15 3 11'), 'Failed - test1'
     print('Done!')
 
