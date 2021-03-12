7e81b5616c6853b57d73150610fe15e926a7c23e
alvik1975 <76609311+alvik1975@users.noreply.github.com>
2021-03-12 23:09:05 +0300

    add task8

---
+++ Homework2/task8.py
@@ -0,0 +1,19 @@
+# 1 - 8 kyu
+# Complete the solution so that it reverses the string passed into it.
+# str_ = input()
+# str_convert = str_[::-1]
+# print('исходный: ', str_, '\nперевернутый: ', str_convert)
+
+# 2 - 8 kyu
+# Complete the solution so that it reverses all of the words within the string passed in.
+# my_str = 'The greatest victory is that which requires no battle'
+# print(*reversed(my_str.split()))
+
+# 3 - 8 kyu
+# Write a function which converts the input string to uppercase.
+def big_letter(str_):
+    print(str_.upper())
+
+
+str_new = input('Введите строку:')
+big_letter(str_new)
