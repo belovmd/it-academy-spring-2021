ed8799e9cd89ff10899bf2724d82f42233b0b681
Uladzimir Soltan <soltanvladimir@yahoo.com>
2021-03-16 20:55:05 +0100

    Fix Homework 2

--- src/Task8.py
+++ src/Task8.py
@@ -54,11 +54,11 @@ def digital_root(n, counter=0):
     n = str(n)
     if len(n) <= 1 and counter > 0:
         return int(n)
-    sum = 0
+    sum_ = 0
     for num in n:
-        sum += int(num)
+        sum_ += int(num)
     counter += 1
-    return digital_root(sum, counter)
+    return digital_root(sum_, counter)
 
 
 """
@@ -82,3 +82,142 @@ def count_smileys(arr):
                    ":-)", ";-)", ":-D", ";-D",
                    ":~)", ";~)", ":~D", ";~D"]
     return len([el for el in arr if el in valid_smile])
+
+
+"""
+Task 5
+
+Implement a function that receives two IPv4 addresses,
+and returns the number of addresses between them
+(including the first one, excluding the last one).
+All inputs will be valid IPv4 addresses in the form of strings.
+The last address will always be greater than the first one.
+"""
+
+
+def ips_between(start, end):
+    st, e = start.split('.'), end.split('.')
+    result = 0
+
+    for i in range(4):
+        result += (int(e[i]) - int(st[i])) * 256 ** (3 - i)
+
+    return result
+
+
+"""
+Task 6
+
+Write a function that accepts a square matrix (N x N 2D array)
+and returns the determinant of the matrix.
+"""
+
+
+def determinant(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    result = 0
+    for y in range(len(matrix)):
+        minor = [matrix[x][:y] + matrix[x][y + 1:] for x in range(1, len(matrix))]
+        mult = -1 if y % 2 else 1
+        result += matrix[0][y] * mult * determinant(minor)
+
+    return result
+
+
+"""
+Task 7
+
+The aim of this kata is to split a given
+string into different strings of equal size
+(note size of strings is passed to the method)
+"""
+
+
+def split_in_parts(s, part_length):
+    result = ''
+    str_to_split = s
+    while len(str_to_split):
+        result += str_to_split[:part_length] + ' '
+        str_to_split = str_to_split[part_length:]
+
+    return result.rstrip()
+
+
+"""
+Task 8
+
+Your job is to write a function which increments a string, to create a new string.
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+"""
+
+
+def increment_string(strng):
+    for i in range(1, len(strng)):
+        if strng[-i].isnumeric():
+            if int(strng[-i]) < 9:
+                return strng[:-i] + str(int(strng[-i:]) + 1)
+        else:
+            if strng[-i + 1] == '9':
+                return strng[:-i + 1] + str(int(strng[-i + 1:]) + 1)
+    return str(int(strng) + 1) if strng.isnumeric() else strng + '1'
+
+
+"""
+Task 9
+
+Pete likes to bake some cakes.
+He has some recipes and ingredients.
+Unfortunately he is not good in maths.
+Can you help him to find out, how many cakes he could bake considering his recipes?
+Write a function cakes(),
+which takes the recipe (object) and the available ingredients (also an object)
+and returns the maximum number of cakes Pete can bake (integer).
+For simplicity there are no units for the amounts
+(e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200).
+Ingredients that are not present in the objects, can be considered as 0.
+"""
+
+
+def cakes(recipe, available):
+    min_ = None
+    for ingredient in recipe:
+        if ingredient not in available:
+            return 0
+        possible_quantity = available[ingredient] // recipe[ingredient]
+        if not min_ or possible_quantity < min_:
+            min_ = possible_quantity
+    return min_
+
+
+"""
+Task 10
+
+Once upon a time, on a way through the old wild mountainous west,…
+… a man was given directions to go from one point to another.
+The directions were "NORTH", "SOUTH", "WEST", "EAST".
+Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+Going to one direction and coming back the opposite direction right away is a needless effort.
+Since this is the wild west, with dreadfull weather and not much water,
+it's important to save yourself some energy, otherwise you might die of thirst!
+"""
+
+
+def dirReduc(arr):
+    i = 0
+    while i < len(arr) - 1:
+        if (arr[i] == 'NORTH' and arr[i + 1] == 'SOUTH' or
+                arr[i] == 'SOUTH' and arr[i + 1] == 'NORTH' or
+                arr[i] == 'WEST' and arr[i + 1] == 'EAST' or
+                arr[i] == 'EAST' and arr[i + 1] == 'WEST'):
+            arr.pop(i)
+            arr.pop(i)
+            i = 0
+        else:
+            i += 1
+    return arr
