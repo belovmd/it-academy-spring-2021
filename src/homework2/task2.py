77cfd274a5650669c727ddadc33e4a7e00825266
Andrei Tsiniakou <a.tsiniakou@gmail.com>
2021-03-17 14:15:51 +0300

    Add task1-7

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -1,9 +1,17 @@
-# Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы.
-# Например, если было введено "abc cde def", то должно быть выведено "abcdef".
+# Найти самое длинное слово в введенном предложении.
+# Учтите что в предложении есть знаки препинания.
 
-string = 'abc cde def'
-newstring = ''
-for char in string.replace(' ',''):
-    if char not in newstring:
-        newstring += char
-print(newstring)
+string = 'Вводится M рублей и N копеек цена, а также количество S товара ' \
+         'Посчитайте общую цену в рублях и копейках за L товаров.'
+znaki_prepinaniya = [',', '.', '?', '-', '!']
+# убираем знаки препинания
+for i in range(0, len(znaki_prepinaniya)):
+    string = string.replace(znaki_prepinaniya[i], '')
+# возвращаем список строк
+new_string = string.split()
+max_len, number = 0, 0
+for i in range(0, len(new_string)):
+    if max_len < len(new_string[i]):
+        max_len = len(new_string[i])
+        number = i
+print('Самое длинное слово:', new_string[number])
