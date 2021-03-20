3766bfb238251f5e3f06be6170facfd21f7cdcfa
Alex Shvedov <alexandershvedov1997@gmail.com>
2021-03-20 16:59:16 +0300

    Add which merge number and chars

--- src/homework2/codewars_task/molecule_to_atoms_5kuy.py
+++ src/homework2/codewars_task/molecule_to_atoms_5kuy.py
@@ -37,6 +37,7 @@ def replace_chars(chr_):
         return ")"
     return chr_
 
+
 def split_string_into_chr(str_):
     lst_ = list(str_)
     lst_of_element = []
@@ -61,6 +62,7 @@ def delete_other_symb(chr_):
         return True
     else:
         return False
+
 
 def merge_molecule_and_digit(new_list):
     i = 0
@@ -87,6 +89,8 @@ def expand_breakets(lst_):
     count_open = sub_lst_.count("(") + 1
     count_close = 1
     previous_pos = 0
+    if count_open == 1:
+        return open_prnth_pos, close_prnth_pos
     while count_open:
         previous_pos = close_prnth_pos
         close_prnth_pos = lst_.index(")", close_prnth_pos + 1)
@@ -94,26 +98,81 @@ def expand_breakets(lst_):
         count_open = count_open + lst_[previous_pos + 1: close_prnth_pos].count("(")
         count_open -= count_close
         count_close = 0
-    return open_prnth_pos , close_prnth_pos -1
+    return open_prnth_pos, close_prnth_pos
+
+
+def merge_molecule_and_digit_expression(lst_, pos):
+    open_prnth, close_prnth = pos
+    if (len(lst_) - 1 > close_prnth) and lst_[close_prnth + 1].isdigit():
+        num = lst_[close_prnth + 1]
+    else:
+        num = 0
+    lst_.pop(open_prnth)
+    lst_.pop(close_prnth - 1)
+    slice_lst = lst_[open_prnth : close_prnth - 1]
+    print(*slice_lst)
+    if num != 0:
+        slice_with_num = add_digit_to_slice(slice_lst, num)
+        result_slice = lst_[: open_prnth] + slice_with_num + lst_[close_prnth : ] # проверить close_prnth может быть последним индексом. Проверка при дебаг
+    else:
+        result_slice = lst_.copy()
+    print(result_slice)
+    return result_slice
+
+
+def add_digit_to_slice(slice_lst, num):
+    open_prnth, close_prnth = 0, 0
+    new_lst = []
+    if slice_lst.count("("):
+        open_prnth, close_prnth  = expand_breakets(slice_lst)
+    i = 0
+    while i < len(slice_lst):
+        if open_prnth and i in range(open_prnth, close_prnth + 1):
+            new_lst.append(slice_lst[i])
+            i += 1
+            continue
+        new_lst.append(slice_lst[i])
+        if (i + 1) < len(slice_lst) and slice_lst[i + 1].isdigit():
+            new_num = int(num) * int(slice_lst[i + 1])
+            new_lst.append(str(new_num))
+            i += 2
+        else:
+            new_lst.append(str(num))
+            i += 1      #мб прорустил new_lst.append(num). Посмотреть при отладке
+    return new_lst
+
+
+def kostyl(lst_):
+    i = 0
+    new_lst = []
+    while i < len(lst_):
+        if (i + 1) < len(lst_) and lst_[i].isdigit() and lst_[i + 1].isdigit() :
+            new_num = int(lst_[i]) * int(lst_[i + 1])
+            new_lst.append(str(new_num))
+            i += 2
+        else:
+            new_lst.append(lst_[i])
+            i += 1
+    return new_lst
+
+
 
 def merge_number_to_char(str_):
     symbl_lst = split_string_into_chr(str_)
     parentheses = list(filter(delete_other_symb, symbl_lst))
     new_list = list(map(replace_chars, symbl_lst))
-    print(*new_list)
     count = new_list.count("(")
     if count == 0:
         new_dict = merge_molecule_and_digit(new_list)
-        print(new_dict)
-    if count > 1:
+        return new_dict
+    while count > 0:
         pos = expand_breakets(new_list)
-        new_list.pop(pos[0])
-        new_list.pop(pos[1])
-        print(new_list)
-        pos = expand_breakets(new_list)
-        new_list.pop(pos[0])
-        new_list.pop(pos[1])
-        print(new_list)
+        new_list = merge_molecule_and_digit_expression(new_list, pos)
+        new_list = kostyl(new_list)
+        count -= 1
+    return merge_molecule_and_digit(new_list)
+
+
 
 
 
