80d1acf218121c256bfd56c05dec76abd80367f9
LiakhovichUladzislau <liakhovich.uladzislau@gmail.com>
2021-03-22 20:53:31 +0300

    corrected tasks

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -17,6 +17,10 @@ for num in str_:
         if not int(num) % 2:
             print(num)
 
+
+
+
+
 # An isogram is a word that has no repeating letters, consecutive or non-consecutive.
 # Implement a function that determines whether a string that contains only letters is
 # an isogram. Assume the empty string is an isogram. Ignore letter case.
