a18cc43d6ee988fb13c2a1bd94e604f875858bca
Alex Shvedov <alexandershvedov1997@gmail.com>
2021-03-22 20:44:39 +0300

    flake8 fix

--- src/homework3/task4.py
+++ src/homework3/task4.py
@@ -11,7 +11,7 @@ def calculate_count_of_pairs(count_num):
     return count_of_pairs
 
 
-def find_repeat_numbers(lst_ ):
+def find_repeat_numbers(lst_):
     repeat_numbers = []
     pair_of_numbers = dict()
     for elem in lst_:
@@ -24,7 +24,7 @@ def find_repeat_numbers(lst_ ):
 
 def print_pair_of_numbers(pair_of_numbers):
     print()
-    print("|{column1:^16}|{column2:^16}|".format(column1= "Элемент", column2="Количество пар"))
+    print("|{column1:^16}|{column2:^16}|".format(column1="Элемент", column2="Количество пар"))
     for key, item in pair_of_numbers.items():
         print("|{key:^16}|{item:^16}|".format(key=key, item=item))
 
