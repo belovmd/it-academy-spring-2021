d7f926aedf5524e5ba4775b06718d20f9c5059c3
nataliasipenko <natalia.asipenko@gmail.com>
2021-03-23 18:18:26 +0300

    new

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -69,7 +69,7 @@ def create_phone_number(n):
 Sum of Digits / Digital Root
 
 Given n, take the sum of the digits of n. If that value has more than one digit,
-continue reducing in this way until a single-digit number is produced. 
+continue reducing in this way until a single-digit number is produced.
 The input will be a non-negative integer.
 """
 
