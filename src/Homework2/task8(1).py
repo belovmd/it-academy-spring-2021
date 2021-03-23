da6edd1b8aac290179aa52895d87fcdd40854198
Serzhevorot <79727116+Serzhevorot@users.noreply.github.com>
2021-03-23 22:33:02 +0300

    Update task8(1).py

--- src/Homework2/task8(1).py
+++ src/Homework2/task8(1).py
@@ -3,7 +3,8 @@
 # you are required to find the runner-up score. You are given  scores.
 # Store them in a list and find the score of the runner-up.
 # Input Format:
-# The first line contains n. The second line contains an array A[] of n integers each separated by a space.
+# The first line contains n. The second line contains an array A[] of n
+# integers each separated by a space.
 # Output Format:
 # Print the runner-up score.
 # Ссылка:
