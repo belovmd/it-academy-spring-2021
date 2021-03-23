b5213fc49a07d97c183e3117f856e3728d7e40a6
nataliasipenko <natalia.asipenko@gmail.com>
2021-03-23 18:15:39 +0300

    homework2

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -2,15 +2,19 @@
 6 kyu
 Vasya - Clerk
 
-The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing
-in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+The new "Avengers" movie has just been released! There are a lot of people
+at the cinema box office standing in a huge line. Each of them has a single 100,
+50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
 
-Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+Vasya is currently working as a clerk. He wants to sell a ticket to every
+single person in this line.
 
-Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly
+Can Vasya sell a ticket to every person and give change if he initially
+has no money and sells the tickets strictly
 in the order people queue?
 
-Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment.
+Return YES, if Vasya can sell a ticket to every person and give change with
+the bills he has at hand at that moment.
 Otherwise return NO.
 """
 
@@ -65,7 +69,8 @@ def create_phone_number(n):
 Sum of Digits / Digital Root
 
 Given n, take the sum of the digits of n. If that value has more than one digit,
-continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+continue reducing in this way until a single-digit number is produced. 
+The input will be a non-negative integer.
 """
 
 
@@ -94,9 +99,11 @@ def digital_root(n):
 6 kyu
 Multiples of 3 or 5
 
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ we get 3, 5, 6 and 9.
 The sum of these multiples is 23.
-Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+Finish the solution so that it returns the sum of all the multiples
+ of 3 or 5 below the number passed in.
 Note: If the number is a multiple of both 3 and 5, only count it once.
 Also, if a number is negative, return 0(for languages that do have them)
 
