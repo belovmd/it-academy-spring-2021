d6001109a24555381c4a023705b33606a2908f66
Serzhevorot <79727116+Serzhevorot@users.noreply.github.com>
2021-03-26 22:34:48 +0300

    Delete src/Homework1 directory

--- src/Homework1/line_6.py
+++
@@ -1,6 +0,0 @@
-import re
-for test_string in ['555-1212', 'ILL-EGAL']:
-    if not re.match(r'^\d{3}-\d{4}$', test_string):
-        print(test_string, 'rejected')
-    else:
-        print(test_string, 'is a valid US local phone number')
