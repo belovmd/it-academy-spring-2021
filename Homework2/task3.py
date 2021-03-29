d9068079ac21eac428e7c1fe686dab92349a9706
alvik1975 <76609311+alvik1975@users.noreply.github.com>
2021-03-29 22:43:46 +0300

    HomeWork2 - correct errors in task1, task3, task5

--- Homework2/task3.py
+++ Homework2/task3.py
@@ -1,6 +1,7 @@
 # Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы
 str = input()
 new_str = []
+
 for i in str:
     if i not in new_str and i != ' ':
         print(i, end='')
