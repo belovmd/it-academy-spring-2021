f89b7bb243f29d56a57401d44f61c12a807bad6f
humqad <humqad@gmail.com>
2021-03-30 04:17:08 +0300

    task4: add whitespace around arithmetic operator

--- src/homework3/task4.py
+++ src/homework3/task4.py
@@ -4,5 +4,5 @@
 str_ = "1 1 2 2 2 3 3 3 3 4 4 4 4 5"
 
 lst_ = [int(x) for x in str_.split()]
-dct_ = {i: (lst_.count(i) * (lst_.count(i)-1) // 2) for i in lst_}
+dct_ = {i: (lst_.count(i) * (lst_.count(i) - 1) // 2) for i in lst_}
 print(dct_)
