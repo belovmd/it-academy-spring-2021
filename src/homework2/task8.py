8395ce6386b7265764b1e6c70ac013fceacaa27b
Maksim_Kozlov <maksimkozlov89@gmail.com>
2021-03-31 11:55:24 +0300

    fix1

--- src/homework2/task8.py
+++ src/homework2/task8.py
@@ -6,7 +6,8 @@ def presses(phrase):
     candybar-shaped feature phones with 3x4 numeric keypads.
     """
 
-    keyboard = {'1', 'ABC2', 'DEF3', 'GHI4', 'JKL5', 'MNO6', 'PQRS7', 'TUV8', 'WXYZ9', ' 0', '#', '*'}
+    keyboard = {'1', 'ABC2', 'DEF3', 'GHI4', 'JKL5',
+                'MNO6', 'PQRS7', 'TUV8', 'WXYZ9', ' 0', '#', '*'}
     count = 0
     for k in phrase.upper():
         for el in keyboard:
@@ -50,7 +51,8 @@ def alphabet_position(text):
     """
     alphabet = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8,
                 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16,
-                'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
+                'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23,
+                'x': 24, 'y': 25, 'z': 26}
 
     result = ''
 
