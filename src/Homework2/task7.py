db9c37dda64432ef3843e32f39475cdf59f8d36c
Maria Shumskaya <shumskaya.maria@gmail.com>
2021-04-03 21:37:14 +0300

    Fixing line break error

--- src/Homework2/task7.py
+++ src/Homework2/task7.py
@@ -1,15 +1,18 @@
 # Task 7. Даны: три стороны треугольника. Требуется: проверить, действительно
 # ли это стороны треугольника. Если стороны определяют треугольник, найти его
-# площадь. Если нет, то вывести сообщение о неверных данных
+# площадь. Если нет, то вывести сообщение о неверных данных.
 
 side_a, side_b, side_c = [int(num) for num in input("Введите 3 числа через "
                                                     "пробел: ").split()]
 
 if side_a + side_b > side_c and side_a + side_c > side_b and \
         side_b + side_c > side_a:
-    semiperimeter = (side_a + side_b + side_c) / 2
-    area = (semiperimeter * (semiperimeter - side_a) *
-            (semiperimeter - side_b) * (semiperimeter - side_c)) ** 0.5
+    semi = (side_a + side_b + side_c) / 2
+
+    # semiperimeter is shortened to "semi" because a longer word causes
+    # a "line break" error in the next line that I couldn't fix.
+
+    area = (semi * (semi - side_a) * (semi - side_b) * (semi - side_c)) ** 0.5
     print("Площадь треугольника =", area)
 
 else:
