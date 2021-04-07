102331bde881087db8f3b3fd5290b2de22c4e472
Maksim_Kozlov <maksimkozlov89@gmail.com>
2021-04-07 14:24:32 +0300

    first commit

---
+++ src/homework4/task2.py
@@ -0,0 +1,36 @@
+"""
+Городf
+
+Дан список стран и городов каждой страны.
+Затем даны названия городов.
+Для каждого города укажите, в какой стране он находится.
+Входные данные
+Программа получает на вход количество стран N.
+Далее идет N строк, каждая строка начинается с названия страны,
+затем идут названия городов этой страны.
+В следующей строке записано число M,
+далее идут M запросов — названия каких-то M городов,
+перечисленных выше.
+Выходные данные
+Для каждого из запроса выведите название страны,
+в котором находится данный город.
+"""
+
+
+num_of_countries = int(input())
+data = {}
+while num_of_countries > 0:
+    line = input().split()
+    data[line[0]] = line[1:]
+    num_of_countries -= 1
+
+requests = []
+num_of_requests = int(input())
+while num_of_requests:
+    requests.append(input())
+    num_of_requests -= 1
+
+for request in requests:
+    for country in data.keys():
+        if request in data[country]:
+            print(country)
---
+++ src/homework4/task5.py
@@ -0,0 +1,35 @@
+"""
+Языки
+
+Каждый из N школьников некоторой школы знает Mi языков.
+Определите, какие языки знают все школьники и языки,
+которые знает хотя бы один из школьников.
+"""
+
+num_of_schoolboys = int(input())
+schoolboy_lang = {}
+all_lang = set()
+
+
+while num_of_schoolboys:
+    num_of_lang = int(input())
+    schoolboy_lang[num_of_schoolboys] = set()
+    while num_of_lang:
+        lang_ = input()
+        all_lang.add(lang_)
+        schoolboy_lang[num_of_schoolboys].add(lang_)
+        num_of_lang -= 1
+    num_of_schoolboys -= 1
+
+
+general_lang = all_lang.copy()
+for lang in schoolboy_lang.values():
+    general_lang &= lang
+
+un_lang = all_lang.copy()
+for lang in schoolboy_lang.values():
+    un_lang |= lang
+
+
+print(len(general_lang), *general_lang, sep='\n')
+print(len(un_lang), *un_lang, sep='\n')
