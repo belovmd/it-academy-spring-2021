201f48863037f0a44e003cf06720f8749d021cee
Uladzimir Soltan <soltanvladimir@yahoo.com>
2021-04-10 22:26:36 +0200

    Remove old Homework 2

--- src/Task6.py
+++
@@ -1,9 +0,0 @@
-def isPalindrome(num):
-    value_to_divide = num
-    value_to_check = 0
-
-    while value_to_divide > 0:
-        value_to_check = value_to_check * 10 + (value_to_divide % 10)
-        value_to_divide //= 10
-
-    return value_to_check == num
--- src/Task7.py
+++
@@ -1,8 +0,0 @@
-def is_triangle(a, b, c):
-    if (a + b > c) and (a + c > b) and (b + c > a):
-        p = (a + b + c) / 2
-        s_squared = p * (p - a) + (p - b) * (p - c)
-        s = s_squared ** 0.5
-        print(f"Площадь треугольника равна {s}")
-    else:
-        print("Перепроверьте данные")
