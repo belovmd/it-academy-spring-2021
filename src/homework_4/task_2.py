a04c43cc9001922f6c5edb3f6a1e0a228b49246d
skolb1992 <s.kolb1992@yandex.ru>
2021-04-13 00:07:16 +0300

    task_5 completed

--- src/homework_4/task_2.py
+++ src/homework_4/task_2.py
@@ -3,3 +3,15 @@
 # названия городов этой страны. В следующей строке записано число M, далее идут
 # M запросов — названия каких-то M городов, перечисленных выше.
 
+5
+Russia Moscow Petersburg Novgorod Kaluga
+Ukraine Kiev Donetsk Odessa
+France Paris Brest
+Hungary Budapest Dior Heviz
+Belarus Minsk Mogilev Brest Grodno
+5
+Odessa
+Moscow
+Novgorod
+Brest
+Dior
