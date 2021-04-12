d0e753aaa55bdd1e5930d1130abd842d375486e4
skolb1992 <s.kolb1992@yandex.ru>
2021-04-13 00:01:48 +0300

    task_5 completed

--- src/homework_4/task_5.py
+++ src/homework_4/task_5.py
@@ -1,4 +1,36 @@
 # Каждый из N школьников некоторой школы знает Mi языков.
 # Определите, какие языки знают все школьники и языки,
 # которые знает хотя бы один из школьников.
+# Входные данные
+# Первая строка входных данных содержит количество школьников N.
+# Далее идет N чисел Mi, после каждого из чисел идет Mi строк, содержащих
+# названия языков, которые знает i-й школьник.
 
+# Пример входных данных:
+# 	3          # N количество школьников
+# 2          # M1 количество языков первого школьника
+# Russian    # языки первого школьника
+# English
+# 3          # M2 количество языков второго школьника
+# Russian
+# Belarusian
+# English
+
+lang = []
+number_pupil = int(input("Введите количество школьников:"))
+pupil_experience = {i: [] for i in list(range(1, number_pupil + 1))}
+
+for key_pupil in range(1, number_pupil + 1):
+    number_language = int(input("Количество языков школьника:"))
+    for key_languages in range(1, number_language + 1):
+        pupil_experience[key_pupil].append(input("Какой язык?:"))
+
+for key in pupil_experience.values():
+    lang.extend(key)
+
+print(*list(set(lang)), sep=', ')
+
+
+for uniq in lang:
+    if lang.count(uniq) == 1:
+        print(uniq)
