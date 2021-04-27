fb5f657f93f3c67baed87a1fb483ebe3a640f54e
Sokol Vladimir <sokolvladimir-by@mail.ru>
2021-04-27 23:40:07 +0300

    Homework-3

--- src/homework3/task1.py
+++ src/homework3/task1.py
@@ -1,5 +1,9 @@
+# FizzBuzz
+# Напишите программу, которая печатает цифры от 1 до 100, но вместо чисел, кратных 3 пишет Fizz,
+# вместо чисел кратный 5 пишет Buzz, а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
+
 for num in range(1, 101):
-    if not num % 3 and not num % 5:
+    if not num % 15:
         print('FizzBuzz')
     elif not num % 3:
         print('Fizz')
--- src/homework3/task2.py
+++ src/homework3/task2.py
@@ -1,10 +1,17 @@
+# List practice
+# Используйте генератор списков чтобы получить следующий: ['ab', 'ac', 'ad', 'bb', 'bc', 'bd'].
+# Используйте на предыдущий список slice чтобы получить следующий: ['ab', 'ad', 'bc'].
+# Используйте генератор списков чтобы получить следующий ['1a', '2a', '3a', '4a'].
+# Одной строкой удалите элемент  '2a' из прошлого списка и напечатайте его.
+# Скопируйте список и добавьте в него элемент '2a' так чтобы в исходном списке этого элемента не было.
+
 import copy
 
-lst_1 = [smbl_lst_1 + smbl_lst_2 for smbl_lst_1 in ['a', 'b'] for smbl_lst_2 in ['b', 'c', 'd']]
+lst_1 = [smbl_lst_1 + smbl_lst_2 for smbl_lst_1 in ['a', 'b'] for smbl_lst_2 in 'bcd']
 print(lst_1)
-lst_2 = [lst_1[num_lst] for num_lst in range(len(lst_1)) if not num_lst % 2]
+lst_2 = lst_1[0:6:2]
 print(lst_2)
-lst_3 = [str(i) + 'a' for i in range(1, 5)]
+lst_3 = [str(i) + 'a' for i in '1234']
 print(lst_3)
 print(lst_3.pop(1))
 lst_4 = copy.deepcopy(lst_3)
