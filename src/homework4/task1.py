c4029abfe4095115f2297d06d45e281c639466b2
Alex Shvedov <alexandershvedov1997@gmail.com>
2021-04-30 19:21:22 +0300

    Add description task

--- src/homework4/task1.py
+++ src/homework4/task1.py
@@ -1,3 +1,10 @@
+'''
+Создайте словарь с помощью генератора словарей,
+так чтобы его ключами были числа от 1 до 20,
+а значениями кубы этих чисел.
+'''
+
+
 dict_ = {elem: elem**3 for elem in range(1, 21)}
 for key, value in dict_.items():
     print("{key} : {value}".format(key=key, value=value), end=" | ")
