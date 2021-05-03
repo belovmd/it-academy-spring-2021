cb573d9a92e1173bdc4912daaebeb44c5534ab7a
Dmitry Subbotin <sduslav@gmail.com>
2021-05-03 21:29:04 +0300

    Improve algorithmic complexity

--- src/homework3/task4.py
+++ src/homework3/task4.py
@@ -1,7 +1,5 @@
 """Homework 3 - Task 4"""
 
-from math import factorial
-
 
 def pairs_number(lst: list) -> int:
     """Пары элементов
@@ -13,27 +11,17 @@ def pairs_number(lst: list) -> int:
     Выходные данные: количество пар.
     Важно: 1 1 1 - это 3 пары, 1 1 1 1 - это 6 пар
     """
-    ret_val = 0
-    elements_counts = {}
+    counts = {}
+    pairs_sum = 0
     for element in lst:
-        elements_counts[element] = elements_counts.get(element, 0) + 1
-    for count in elements_counts.values():
-        if count > 1:
-            ret_val += combinations_without_repetitions(count, 2)
-    return ret_val
-
-
-def combinations_without_repetitions(n: int, k: int) -> int:
-    """Число сочетаний без повторений из n по к
-
-    Генерирует исключение ValueError, если n < k.
-    """
-    if n < k:
-        raise ValueError('n < k')
-    return int(factorial(n) / (factorial(n - k) * factorial(k)))
+        counts[element] = counts.get(element, 0) + 1
+        pairs_sum += counts[element] - 1
+    return pairs_sum
 
 
 if __name__ == '__main__':
     assert pairs_number([1, 1, 1]) == 3, 'failed!'
     assert pairs_number([1, 1, 1, 1]) == 6, 'failed!'
+    assert pairs_number([1, 1, 1, 1, 1]) == 10, 'failed!'
+    assert pairs_number([1, 1, 1, 2, 2, 3]) == 4, 'failed!'
     print(pairs_number([1, 1, 1, 2, 2, 3]))
