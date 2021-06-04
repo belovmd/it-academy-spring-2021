776b66527fb70fb4ca945f6d275cf774008c9825
Maria Shumskaya <shumskaya.maria@gmail.com>
2021-06-04 15:18:50 +0300

    not working task1

---
+++ src/homework5/my_funcs.py
@@ -0,0 +1,28 @@
+def histogram(lst):
+    dict_ = {}
+    for item in lst:
+        if item in dict_.keys():
+            dict_[item] += 1
+        else:
+            dict_[item] = 1
+    for keys in sorted(dict_.keys(), reverse=True):
+        print("{}: {}".format(keys, "*" * dict_[keys]))
+
+def stars(num):
+    print(num * "*")
+
+def blocks(str_):
+    print(len(str_))
+def non():
+    print("a")
+
+years = [1994, 1972,1974,2008,1957,1993,1994,2003]
+ratings = [9.2,9.2,9.0,9.0,8.9,8.9,8.9,8.9,8.8,8.8]
+
+histogram(years)
+histogram(ratings)
+stars(5)
+stars(7)
+blocks('name')
+blocks('names and years')
+
---
+++ src/homework5/task1.py
@@ -0,0 +1,23 @@
+# Task 1 — Оформите решение задач из прошлых домашних работ в функции.
+# Напишите функцию runner. (все станет проще когда мы изучим модули, getattr и
+# setattr)
+# runner() – все фукнции вызываются по очереди
+# runner(‘func_name’) – вызывается только функцию func_name.
+# runner(‘func’, ‘func1’...) - вызывает все переданные функции
+
+from my_funcs import histogram, stars, blocks
+
+funcs = (my_funcs.histogram(), my_funcs.stars(), my_funcs.blocks())
+
+
+def runner(*args):
+    for func in funcs:
+        attr = getattr(my_funcs, func)
+        if not args:
+            return attr
+        else:
+            return funcs
+
+runner()
+runner("stars")
+runner("stars", "blocks")
