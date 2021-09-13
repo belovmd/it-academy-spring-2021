284d0d06bab283b55837c697e1d365f1f9bb32d3
Sokol Vladimir <sokolvladimir-by@mail.ru>
2021-09-13 12:58:36 +0300

    hw4

--- src/homework4/task7.py
+++ src/homework4/task7.py
@@ -1,21 +1,17 @@
 # Даны два натуральных числа. Вычислите их наибольший общий делитель при помощи
 # алгоритма Евклида (мы не знаем функции и рекурсию).
 
-first_num = 12312
-second_num = 4236546456
+first_num = int(input('Введите число №1 '))
+second_num = int(input('Введите число №2 '))
 
-if first_num > second_num:
+if first_num < second_num:
+    first_num, second_num = second_num, first_num
+elif first_num == second_num:
+    print("НОД = ", first_num)
 
-    while first_num and second_num:
-
-        first_num = first_num % second_num
-        second_num = second_num % first_num
-
-else:
-
-    while first_num and second_num:
-
-        second_num = second_num % first_num
-        first_num = first_num % second_num
-
-print(first_num, second_num)
+while first_num > second_num:
+    first_num = first_num % second_num
+    if first_num:
+        first_num, second_num = second_num, first_num
+    else:
+        print("НОД = ", first_num + second_num)
