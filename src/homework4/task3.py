ad409c9d9cde9072baa703f7d659e3eb8beee608
Sokol Vladimir <sokolvladimir-by@mail.ru>
2021-09-27 00:23:05 +0300

    hw_5

--- src/homework4/task3.py
+++ src/homework4/task3.py
@@ -1,11 +1,28 @@
-# Даны два списка чисел. Посчитайте, сколько различных чисел содержится одновременно как в
-# первом списке, так и во втором.
+# Реализовать функцию get_ranges которая получает на вход непустой список
+# неповторяющихся целых чисел, отсортированных по возрастанию,
+# которая этот список “сворачивает”
+#  get_ranges([0, 1, 2, 3, 4, 7, 8, 10]) // "0-4,7-8,10"
+#  get_ranges([4,7,10]) // "4,7,10"
+#  get_ranges([2, 3, 8, 9]) // "2-3,8-9"
 
-lst_1 = (1, 3, 12, 17, 7, 2)
-lst_2 = (4, 6, 2, 4, 6, 9, 8)
+# Аларм!!! Я немного добавил функционала. Функция получает позиционные аргументы в хаотичном
+# виде, удаляет повторяющиеся числа и сортирует их.
 
-lst_1, lst_2 = set(lst_1), set(lst_2)
+def get_ranges(*args):
+  set_ = set(args)
+  num_list = list(set_)
+  num_list.sort()
+  str_ = ""
+  len_args = len(num_list)
+  for num in range(len_args - 1):
+    if (num_list[num] - 1) != num_list[num-1] and (num_list[num] + 1) == num_list[num + 1]:
+      str_ += str(num_list[num]) + '-'
+    elif (num_list[num] - 1) == num_list[num-1] and (num_list[num] + 1) == num_list[num + 1]:
+      continue
+    else:
+      str_ += str(num_list[num]) + ','
+  str_ += str(num_list[len_args - 1])
+  return str_
 
-set_1 = lst_1 ^ lst_2
 
-print(len(set_1))
+get_ranges(3, 46, 7, 8, 0, 2, 5, 4, 7, 54, 76, 3, 54, 6, 76, 57, 58, 3)
