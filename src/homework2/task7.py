0f98e8550da834df6d91dda149e37fb4f616a7ca
Ivan <ivanrakach@gmail.com>
2021-11-02 18:50:48 +0300

    small adjustments - 6

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -11,11 +11,11 @@ C = 10
 # p = (A+B+C)//2 # semi-perimeter
 # S = (abs(p*(p-A)*(p-B)*(p-C)))**0.5 # area
 if A < B + C and B < A + C and C < A + B:
-    print("A, B, C - sides of a triangle")
+    # print("A, B, C - sides of a triangle")
     # print ((A + B + C) // 2)
-    print("Area of a triangle:", (abs(
-        ((A + B + C) // 2)
-        * (((A + B + C) // 2) - A) * (((A + B + C) // 2) - B)
-        * (((A + B + C) // 2) - C))) ** 0.5)
+    print("Area of a triangle:", (
+        abs(((A + B + C) // 2)
+            * (((A + B + C) // 2) - A) * (((A + B + C) // 2) - B)
+            * (((A + B + C) // 2) - C))) ** 0.5)
 else:
     print("Wrong data")
